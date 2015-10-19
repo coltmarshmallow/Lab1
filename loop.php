7ab57e4bf5d0184d8e163f1f95742bdb764f4bc2
coltmarhsmallow <josh.lees@hotmail.com>
2015-10-19 15:43:37 +0100

    first test lab 1

---
+++ loop.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <body>
+            <p>
+                <?php
+
+
+                $myage = 18;
+
+                if ($myage > 16){
+                    print "You cans buy specs";
+                } else if ($myage > 18){
+                    print "You cans buy mugs";
+                } else if ($myage > 21){
+                    print "You cans buy sausage rolls";
+                }
+
+
+                /**
+                 * Created by PhpStorm.
+                 * User: 1301657
+                 * Date: 19/10/2015
+                 * Time: 15:38
+                 */
+                ?>
+            </p>
+        </body>
+    </head>
+</html>
