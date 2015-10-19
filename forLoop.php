6b5d360de52b7622f93ddf3504d042d8046828dd
coltmarhsmallow <josh.lees@hotmail.com>
2015-10-19 16:26:55 +0100

    first test lab 1

---
+++ forLoop.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<body>
+<p>
+    <?php
+
+        for ($i = 1; $i < 30; $i++){
+            if ($i % 4 == 0){
+                echo "<p>Specs are available</p>";
+                echo "<p>Sausage rolls are available</p>";
+            } else if ($i % 3 == 0) {
+                echo "<p>Mugs are available</p>";
+            } else if ($i % 2 == 0) {
+                echo "<p>Specs are available</p>";
+            } else {
+                echo "<p>Nothing is available</p>";
+            }
+    }
+
+
+    ?>
+</p>
+</body>
+</head>
+</html>
