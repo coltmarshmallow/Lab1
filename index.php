ffd617252744deb14a38e4f38b3e01efd9a16b81
coltmarhsmallow <josh.lees@hotmail.com>
2015-10-19 15:36:08 +0100

    first test lab 1

--- index.php
+++ index.php
@@ -12,6 +12,29 @@
                 $myage = 111;
                 echo "My name is " . $myname . " and I am " . $myage;
 
+                $name = "Edgar";
+                if ($name == "Simon"){
+                    print "I know you!";
+                } else {
+                    print "Who are you?";
+                }
+
+                $numberOfHobbits = 2;
+
+                switch($numberOfHobbits) {
+                    case 1:
+                        echo "1 sad hobbit";
+                        break;
+                    case 2:
+                        echo "2 happy hobbits";
+                        break;
+                    case 3:
+                        echo "3 hobbits are a crowd";
+                        break;
+                    default:
+                        echo "All the hobbits have gone home";
+                }
+
                 ?>
             </p>
         </body>
