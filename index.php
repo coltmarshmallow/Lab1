388bb12577e165e41b5f9ed24dbd670bea48105b
coltmarhsmallow <josh.lees@hotmail.com>
2015-10-19 15:22:12 +0100

    first test lab 1

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
                 echo 5 * 7;
 
                 $myname = "Frodo Baggins";
+                $myage = 111;
+                echo "My name is" . $myname . "and I am " . $myage;
 
                 ?>
             </p>
