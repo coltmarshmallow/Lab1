e251012872443e1db5d0eecc92f7911b37539b09
unknown <1301657@RG41115.rgu.ac.uk>
2015-10-19 14:49:28 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <body>
+            <p>
+                <?php
+                echo "Hello World";
+                ?>
+            </p>
+    </body>
+    </head>
+</html>
+
+
+/**
+ * Created by PhpStorm.
+ * User: 1301657
+ * Date: 19/10/2015
+ * Time: 14:28
+ */
